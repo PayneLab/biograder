1d7843da27fc1ee907b7cde7254e07f85bcc3eae
Calvin Woods <caldogwoods@byu.edu>
2021-01-26 18:33:48 -0700

    v 1.0.6 more collabs debugging

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(readme_path) as readme_file:
 
 setup(
     name='biograder',
-    version='1.0.5',
+    version='1.0.6',
     author='PayneLab',
     author_email='sam_payne@byu.edu',
     description='grader',
