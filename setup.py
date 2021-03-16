a8204165685cc4676bf5e2800e42ee137adaabec
Calvin Woods <caldogwoods@byu.edu>
2021-03-16 15:49:02 -0600

    pip v 0.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(readme_path) as readme_file:
 
 setup(
     name='biograder',
-    version='1.1.12',
+    version='0.1.0',
     author='PayneLab',
     author_email='sam_payne@byu.edu',
     description='grader',
